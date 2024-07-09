10d60ef5ba7bb5fe3c1be5047a343ce14977a4f7
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2024-07-10 04:29:48 +0530

    UI fix

---
+++ src/app/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="fixed inset-x-0 bottom-0 bg-white text-center p-4">
+            <p>Made with 🏋️ by <a href="https://github.com/shivamvishwakarm" className="text-blue-500 underline hover:text-blue-700">Shivam Vishwakarma</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react';
 import staticResults from '../../public/results.json';
+import Footer from './Footer';
 
 export default function Home() {
     const [name, setName] = useState('');
@@ -13,32 +14,35 @@ export default function Home() {
         setResults(filteredResults);
     };
     return (
-        <div>
-            <h1>Search Your Results</h1>
+        <div className='flex flex-col font-mono justify-center items-center'>
+            <h1 className='text-2xl mt-2 font-bold text-white gradiant'>Welcome to Bsc.IT Result Portal</h1>
+            
             <input
+                className=' p-2 w-1/2 mt-4'
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                placeholder="Enter your name"
+                placeholder="Search your result by name only"
             />
-            <button onClick={searchResults}>Search</button>
-            <div id="results">
+            <button className='border  text-white px-4 py-2 rounded mt-2 hover:bg-black' onClick={searchResults}>Search</button>
+            <div id="results" className='flex flex-wrap text-white p-4  '>
                 {results.length === 0 ? (
-                    <p>No results found.</p>
+                    <p>NO Result</p>
                 ) : (
                     results.map((result, index) => (
-                        <div key={index}>
+                        <div className='border rounded-md p-4 m-2 bg-[#5c52a2] w-full' key={index}>
+                            <h2 className='text-xl font-bold text-[#030637]'>Name: {result.name}</h2>
                             <p>Roll No: {result.roll_no}</p>
-                            <p>Name: {result.name}</p>
                             <p>Marks in 3rd year: {result.total_marks_3rd}/500</p>
                             <p>Total marks in Honours: {result.total_marks_hons}/800</p>
                             <p>Division: {result.division}</p>
                             <p>Grand Total: {result.grand_total}/1500</p>
-                            <hr />
+                         
                         </div>
                     ))
                 )}
             </div>
+            <Footer/>
         </div>
     );
 }
